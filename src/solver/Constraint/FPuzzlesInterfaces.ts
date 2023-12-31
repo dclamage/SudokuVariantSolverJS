aa0fd5bdf474ee752ea05c4abd3acd0f63d9d537
David Clamage <rangsk@gmail.com>
2023-12-30 20:35:54 -0700

    FPuzzlesBoard

--- src/solver/Constraint/FPuzzlesInterfaces.ts
+++ src/solver/Constraint/FPuzzlesInterfaces.ts
@@ -10,6 +10,7 @@ export interface FPuzzlesKillerCageEntry {
 
 export interface FPuzzlesLittleKillerSumEntry {
     cell: string;
+    cells: string[];
     direction: string;
     value: string;
 }
@@ -37,4 +38,61 @@ export interface FPuzzlesClone {
 export interface FPuzzlesQuadruple {
     cells: string[];
     values: number[];
+}
+
+export interface FPuzzlesGridEntry {
+    value?: number;
+    given?: boolean;
+    centerPencilMarks?: number[];
+    givenPencilMarks?: number[];
+    region?: number;
+}
+
+export interface FPuzzlesBoard {
+    size: number;
+    title?: string;
+    author?: string;
+    ruleset?: string;
+    grid: FPuzzlesGridEntry[][];
+    diagonalp?: boolean;
+    diagonaln?: boolean;
+    antiknight?: boolean;
+    antiking?: boolean;
+    disjointgroups?: boolean;
+    nonconsecutive?: boolean;
+    negative?: string[];
+    arrow?: FPuzzlesArrowEntry[];
+    killercage?: FPuzzlesKillerCageEntry[];
+    cage?: FPuzzlesKillerCageEntry[];
+    littlekillersum?: FPuzzlesLittleKillerSumEntry[];
+    odd?: FPuzzlesCell[];
+    even?: FPuzzlesCell[];
+    minimum?: FPuzzlesCell[];
+    maximum?: FPuzzlesCell[];
+    rowindexer?: FPuzzlesCells[];
+    columnindexer?: FPuzzlesCells[];
+    boxindexer?: FPuzzlesCells[];
+    extraregion?: FPuzzlesCells[];
+    thermometer?: FPuzzlesLines[];
+    palindrome?: FPuzzlesLines[];
+    renban?: FPuzzlesLines[];
+    whispers?: FPuzzlesLines[];
+    regionsumline?: FPuzzlesLines[];
+    difference?: FPuzzlesCells[];
+    xv?: FPuzzlesCells[];
+	sum? : FPuzzlesCells[];
+    ratio?: FPuzzlesCells[];
+    clone?: FPuzzlesClone[];
+    quadruple?: FPuzzlesQuadruple[];
+    betweenline?: FPuzzlesLines[];
+    sandwichsum?: FPuzzlesCell[];
+    xsum?: FPuzzlesCell[];
+    skyscraper?: FPuzzlesCell[];
+    entropicline?: FPuzzlesLines[];
+    modularline?: FPuzzlesLines[];
+    nabner?: FPuzzlesLines[];
+    doublearrow?: FPuzzlesLines[];
+    zipperline?: FPuzzlesLines[];
+    disabledlogic?: string[];
+    truecandidatesoptions?: string[];
 }
