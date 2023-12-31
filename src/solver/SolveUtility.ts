cc4ac25fdbc1425cb083084c805337c027c0d0c2
gracefu <81774659+gracefuu@users.noreply.github.com>
2023-12-31 08:03:04 +0800

    Add type annotations for SolveUtility

--- src/solver/SolveUtility.ts
+++ src/solver/SolveUtility.ts
@@ -1,4 +1,6 @@
-export function popcount(x) {
+import { Board } from "./Board";
+
+export function popcount(x: number) :number{
     x -= (x >> 1) & 0x55555555;
     x = (x & 0x33333333) + ((x >> 2) & 0x33333333);
     x = (x + (x >> 4)) & 0x0f0f0f0f;
@@ -8,37 +10,37 @@ export function popcount(x) {
 }
 
 // Count the number of trailing zeros in an integer
-export function ctz(x) {
+export function ctz(x: number): number {
     return popcount((x & -x) - 1);
 }
 
 // Computes the bitmask with all values set
-export function allValues(size) {
+export function allValues(size: number): number {
     return (1 << size) - 1;
 }
 
 // Computes the bitmask with a specific value set
-export function valueBit(value) {
+export function valueBit(value: number): number {
     return 1 << (value - 1);
 }
 
 // Get the value of the first set bit
-export function minValue(bits) {
+export function minValue(bits: number): number {
     return ctz(bits) + 1;
 }
 
 // Get the value of the last set bit
-export function maxValue(bits) {
+export function maxValue(bits: number): number {
     return 32 - Math.clz32(bits);
 }
 
 // Get if a value is set
-export function hasValue(bits, value) {
+export function hasValue(bits: number, value: number): boolean {
     return (bits & valueBit(value)) !== 0;
 }
 
 // Get the value of a randomly set bit
-export function randomValue(bits) {
+export function randomValue(bits: number): number {
     if (bits === 0) {
         return 0;
     }
@@ -57,12 +59,12 @@ export function randomValue(bits) {
     return 0;
 }
 
-export function valuesMask(values) {
+export function valuesMask(values: number[]): number {
     return values.reduce((mask, value) => mask | valueBit(value), 0);
 }
 
-export function valuesList(mask) {
-    const values = [];
+export function valuesList(mask: number): number[] {
+    const values :number[]= [];
     while (mask !== 0) {
         const value = minValue(mask);
         values.push(value);
@@ -71,7 +73,7 @@ export function valuesList(mask) {
     return values;
 }
 
-export function binomialCoefficient(n, k) {
+export function binomialCoefficient(n: number, k: number): number {
     if (k < 0 || k > n) {
         return 0;
     }
@@ -91,7 +93,7 @@ export function binomialCoefficient(n, k) {
     return result;
 }
 
-const combinationIndicesCache = [];
+const combinationIndicesCache:number[][][][] = [];
 
 // Precompute indices for combinations
 for (let n = 1; n <= 9; n++) {
@@ -101,7 +103,7 @@ for (let n = 1; n <= 9; n++) {
     }
 }
 
-function* _generateCombinationIndices(n, k, start = 0, prefix = []) {
+function* _generateCombinationIndices(n: number, k: number, start: number = 0, prefix: number[] = []):Generator<number[] >{
     if (prefix.length === k) {
         yield prefix.slice();
     } else {
@@ -113,7 +115,7 @@ function* _generateCombinationIndices(n, k, start = 0, prefix = []) {
     }
 }
 
-export function* combinationIndices(n, k) {
+export function* combinationIndices(n: number, k: number): Generator<number[], void, undefined> {
     if (n <= 9) {
         yield* combinationIndicesCache[n][k];
     } else {
@@ -121,7 +123,7 @@ export function* combinationIndices(n, k) {
     }
 }
 
-export function* combinations(array, size) {
+export function* combinations<T>(array: T[], size: number): Generator<T[]> {
     if (array.length <= 9) {
         yield* combinationsCached(array, size);
     } else {
@@ -129,8 +131,8 @@ export function* combinations(array, size) {
     }
 }
 
-export function* combinationsUncached(array, size) {
-    function* combine(start, prefix, depth) {
+export function* combinationsUncached<T>(array: T[], size: number): Generator<T[]> {
+    function* combine(start: number, prefix: T[], depth: number) {
         if (depth === size) {
             yield prefix.slice();
         } else {
@@ -145,13 +147,13 @@ export function* combinationsUncached(array, size) {
     yield* combine(0, [], 0);
 }
 
-export function* combinationsCached(array, size) {
+export function* combinationsCached<T>(array: T[], size: number): Generator<T[]> {
     for (const indices of combinationIndicesCache[array.length][size]) {
         yield indices.map(index => array[index]);
     }
 }
 
-export function* permutations(array) {
+export function* permutations<T>(array:T[]):Generator<T[]> {
     const n = array.length;
     let c = new Array(n).fill(0);
 
@@ -173,23 +175,23 @@ export function* permutations(array) {
 }
 
 // Helper for memo keys
-export function cellsKey(prefix, cells, size) {
+export function cellsKey(prefix: string, cells: any, size: any): string {
     return prefix + appendCellNames(cells, size);
 }
 
-export function appendInts(ints) {
+export function appendInts(ints: any[]):string {
     return ints.map(i => '|' + i).join('');
 }
 
-export function appendCellNames(cells, size) {
+export function appendCellNames(cells: number[], size: number): string {
     return cells.map(cell => '|' + cellName(cell, size)).join('');
 }
 
-export function maskToString(mask, size) {
+export function maskToString(mask: number, size: number): string {
     return valuesList(mask).join(size >= 10 ? ',' : '');
 }
 
-export function appendCellValueKey(board, cells) {
+export function appendCellValueKey(board: Board, cells: number[]):string {
     let builder = '';
     cells.forEach(cellIndex => {
         const mask = board.cells[cellIndex];
@@ -198,13 +200,13 @@ export function appendCellValueKey(board, cells) {
     return builder;
 }
 
-export function cellName(cellIndex, size) {
+export function cellName(cellIndex: number, size: number): string {
     const row = Math.floor(cellIndex / size);
     const col = cellIndex % size;
     return `R${row + 1}C${col + 1}`;
 }
 
-export function cellIndexFromName(name, size) {
+export function cellIndexFromName(name: string, size: number): number {
     const regex = /r(\d+)c(\d+)/;
     const match = regex.exec(name.toLowerCase());
     if (!match) {
@@ -216,7 +218,7 @@ export function cellIndexFromName(name, size) {
     return row * size + col;
 }
 
-export function sequenceEqual(arr1, arr2) {
+export function sequenceEqual<T>(arr1: T[], arr2: T[]): boolean {
     if (arr1.length !== arr2.length) {
         return false;
     }
@@ -225,7 +227,7 @@ export function sequenceEqual(arr1, arr2) {
 }
 
 // Assumes arr is sorted
-export function removeDuplicates(arr) {
+export function removeDuplicates<T>(arr: T[]): T[] {
     if (!arr.length) {
         return arr;
     }
