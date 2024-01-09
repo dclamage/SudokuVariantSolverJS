3ef76299c2d7c3db438a24b688863a7f9fdb79b4
gracefu <81774659+gracefuu@users.noreply.github.com>
2024-01-09 10:20:30 +0800

    Convert OrConstraint to V2 (WIP)

--- src/solver/Constraint/OrConstraint.ts
+++ src/solver/Constraint/OrConstraint.ts
@@ -1,12 +1,13 @@
-import { Board } from '../Board';
-import { valueBit, minValue, CellIndex, CellValue, CandidateIndex } from '../SolveUtility';
-import { Constraint, ConstraintResult, InitResult } from './Constraint';
+import { Board, ReadonlyBoard } from '../Board';
+import { valueBit, minValue, CellIndex, CellValue, CandidateIndex, WeakLink } from '../SolveUtility';
+import { Constraint } from './Constraint';
+import { ConstraintV2, ConstraintResult, InitResult, LogicalDeduction } from './ConstraintV2';
 
 interface OrConstraintParams {
     subboards: Board[];
 }
 
-export class OrConstraint extends Constraint {
+export class OrConstraint extends ConstraintV2 {
     numCells: number;
     numCandidates: number;
     subboards: Board[];
@@ -14,14 +15,14 @@ export class OrConstraint extends Constraint {
 
     constructor(constraintName: string, specificName: string, board: Board, params: OrConstraintParams) {
         if (constraintName === undefined || specificName === undefined || board === undefined || params === undefined) {
-            super(undefined, undefined, undefined);
+            super(undefined, undefined);
             this.numCells = undefined;
             this.numCandidates = undefined;
             this.subboards = undefined;
             this.subboardsChanged = undefined;
         } else {
             const { subboards } = params;
-            super(board, constraintName, specificName);
+            super(constraintName, specificName);
             this.numCells = board.size * board.size;
             this.numCandidates = board.size * board.size * board.size;
             this.subboards = subboards;
@@ -29,7 +30,7 @@ export class OrConstraint extends Constraint {
         }
     }
 
-    init(board: Board, isRepeat: boolean): InitResult {
+    init(board: Board): InitResult {
         this.subboards = this.subboards.filter(subboard => {
             // Copy state downwards
             for (let cellIndex = 0; cellIndex < this.numCells; ++cellIndex) {
@@ -41,7 +42,7 @@ export class OrConstraint extends Constraint {
 
             // Init constraints
             // If init fails, filter out the subboard immediately
-            const keepSubboard = subboard.initConstraints(isRepeat);
+            const keepSubboard = subboard.initConstraints();
             if (!keepSubboard) {
                 subboard.release();
             }
@@ -52,53 +53,6 @@ export class OrConstraint extends Constraint {
         if (this.subboards.length === 0) {
             return ConstraintResult.INVALID;
         }
-
-        // Transfer presolve deductions shared by all subboards up
-        let changed = ConstraintResult.UNCHANGED;
-        for (let cellIndex = 0; cellIndex < this.numCells; ++cellIndex) {
-            const cellMask = this.subboards.reduce((mask, subboard) => mask | subboard.cells[cellIndex], 0);
-            const result = board.keepCellMask(cellIndex, cellMask);
-            if (result === ConstraintResult.INVALID) {
-                return ConstraintResult.INVALID;
-            } else if (result === ConstraintResult.CHANGED) {
-                changed = ConstraintResult.CHANGED;
-            }
-        }
-
-        // Transfer weak links shared by all subboards up
-        let scratch: CandidateIndex[] = [];
-        for (let candidate = 0; candidate < this.numCandidates; ++candidate) {
-            // Find the interesction of all subboard weak links for this candidate
-            let newLinks = this.subboards[0].binaryImplications.getTopLayerNegConsequences(candidate);
-
-            for (let subboardIndex = 1; subboardIndex < this.subboards.length; ++subboardIndex) {
-                if (newLinks.length === 0) {
-                    break;
-                }
-                scratch.length = 0;
-                this.subboards[subboardIndex].binaryImplications.filterOutTopLayerNegConsequences(candidate, newLinks, scratch);
-                [newLinks, scratch] = [scratch, newLinks];
-            }
-
-            if (newLinks.length === 0) {
-                continue;
-            }
-
-            changed = ConstraintResult.CHANGED;
-
-            for (const subboard of this.subboards) {
-                for (const link of newLinks) {
-                    subboard.transferWeakLinkToParentSubboard(candidate, link);
-                }
-            }
-        }
-
-        // Only a single subboard, send up all our constraints onto the main board and delete ourselves
-        if (this.subboards.length === 1) {
-            return { result: changed, addConstraints: this.subboards[0].constraints.slice(), deleteConstraints: [this] };
-        }
-
-        return changed;
     }
 
     finalize() {
@@ -168,7 +122,120 @@ export class OrConstraint extends Constraint {
         return true;
     }
 
-    logicStep(board: Board, logicalStepDescription: string[]) {
+    // No obvious steps
+
+    logicalStep(board: ReadonlyBoard): LogicalDeduction[] {
+        this.subboards = this.subboards.filter(subboard => {
+            // Transfer deductions downward
+            for (let cellIndex = 0; cellIndex < this.numCells; ++cellIndex) {
+                subboard.keepCellMask(cellIndex, board.cells[cellIndex]);
+            }
+
+            // Step all subconstraints repeatedly until no more further deductions are possible
+            let changed = false;
+            do {
+                changed = false;
+
+                for (const constraint of subboard.constraints) {
+                    // TODO: Handle v2 constraints in or constraints
+                    // Don't bother with logical deductions made in subboards, they get a bit too noisy to show.
+                    const result = constraint.logicStep(subboard, null);
+                    if (result === ConstraintResult.INVALID) {
+                        // Subboard is broken, filter it out
+                        this.subboardsChanged = true;
+                        subboard.release();
+                        return false;
+                    } else if (result === ConstraintResult.CHANGED) {
+                        this.subboardsChanged = true;
+                        changed = true;
+                    }
+                }
+            } while (changed);
+
+            // Subboard not broken at this point, keep it
+            return true;
+        });
+
+        if (this.subboards.length === 0) {
+            return [
+                {
+                    explanation: 'All cases broken, this Or seemsconstraint cannot be satisfied',
+                    invalid: true,
+                },
+            ];
+        }
+
+        const deductions = [];
+
+        // Transfer deductions shared by all subboards upward
+        const eliminations = [];
+        for (let cellIndex = 0; cellIndex < this.numCells; ++cellIndex) {
+            let cellMask = 0;
+            for (const subboard of this.subboards) {
+                cellMask |= subboard.cells[cellIndex];
+            }
+            let removedMask = board.cells[cellIndex] & board.allValues & ~cellMask;
+            while (removedMask !== 0) {
+                const value = minValue(removedMask);
+                removedMask ^= valueBit(value);
+                eliminations.push(board.candidateIndex(cellIndex, value));
+            }
+        }
+        if (eliminations.length > 0) {
+            deductions.push({
+                explanation: '',
+                eliminations,
+            });
+        }
+
+        // Can't unset subboardsChanged here since the user may not necessarily apply the deductions we suggest
+        // That's fine though, this isn't perf sensitive
+
+        let scratch: CandidateIndex[] = [];
+        const weakLinks: WeakLink[] = [];
+        for (let candidate = 0; candidate < this.numCandidates; ++candidate) {
+            let newLinks = this.subboards[0].binaryImplications.getTopLayerNegConsequences(candidate);
+            for (let subboardIndex = 1; subboardIndex < this.subboards.length; ++subboardIndex) {
+                if (newLinks.length === 0) {
+                    break;
+                }
+                scratch.length = 0;
+                this.subboards[subboardIndex].binaryImplications.filterOutTopLayerNegConsequences(candidate, newLinks, scratch);
+                [newLinks, scratch] = [scratch, newLinks];
+            }
+            for (const link of newLinks) {
+                if (board.isWeakLink(candidate, link)) {
+                    for (const subboard of this.subboards) {
+                        subboard.binaryImplications.graph.unsafeRemoveImplication(candidate, link);
+                    }
+                }
+                weakLinks.push([candidate, link]);
+            }
+        }
+        if (weakLinks.length > 0) {
+            deductions.push({
+                explanation: 'Weak links shared by all subboards',
+                weakLinks,
+            });
+        }
+
+        if (this.subboards.length === 1) {
+            deductions.push({
+                explanation: 'Restricted to only 1 subboard, adding weak links and constraints of subboard to main board',
+                addConstraints: this.subboards[0].constraints.slice(),
+                deleteConstraints: [this],
+                weakLinks: Array.from({ length: board.size * board.size * board.size }, (_, candidate) =>
+                    this.subboards[0].binaryImplications
+                        .getTopLayerNegConsequences(candidate)
+                        .map((link: CandidateIndex): WeakLink => [candidate, link])
+                ).flat(),
+            });
+        }
+
+        return deductions;
+    }
+
+    bruteForceStep(board: Board): ConstraintResult {
         this.subboards = this.subboards.filter(subboard => {
             // Step all subconstraints repeatedly until no more further deductions are possible
             let changed = false;
@@ -195,7 +262,6 @@ export class OrConstraint extends Constraint {
         });
 
         if (this.subboards.length === 0) {
-            logicalStepDescription === null || logicalStepDescription.push('All cases broken, this Or constraint cannot be satisfied.');
             return ConstraintResult.INVALID;
         }
 
@@ -206,27 +272,18 @@ export class OrConstraint extends Constraint {
 
         // Transfer deductions shared by all subboards upward
         let changed = ConstraintResult.UNCHANGED;
-        const elims = [];
         for (let cellIndex = 0; cellIndex < this.numCells; ++cellIndex) {
             let cellMask = 0;
             for (const subboard of this.subboards) {
                 cellMask |= subboard.cells[cellIndex];
             }
-            let removedMask = board.cells[cellIndex] & ~cellMask;
-            while (removedMask !== 0) {
-                const value = minValue(removedMask);
-                removedMask ^= valueBit(value);
-                elims.push(board.candidateIndex(cellIndex, value));
-            }
             const result = board.keepCellMask(cellIndex, cellMask);
             if (result === ConstraintResult.INVALID) {
                 return ConstraintResult.INVALID;
-            } else if (result === ConstraintResult.CHANGED) {
+            }
+            if (result === ConstraintResult.CHANGED) {
                 changed = ConstraintResult.CHANGED;
             }
-        }
-        if (changed && logicalStepDescription !== null) {
-            logicalStepDescription.push(`${board.describeElims(elims)}.`);
         }
 
         return changed;
