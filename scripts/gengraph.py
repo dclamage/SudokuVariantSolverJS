7074b1d21ab8a0f9bcbed9d828d7fad77af7568c
gracefuu <81774659+gracefuu@users.noreply.github.com>
2024-01-31 13:37:44 +0800

    Benchmark improvements (#53)
    * Try to speed up benchmark workflow setup
    
    * Make links point to the specific commit hash rather than just the gh_pages branch
    
    * Put the profiling stats for everything under the same top level node (Timer Fired)
    
    * Make benchmark charts use median guesses/time on the x axis for scatter plots, and only plot the median for the sorted line plot

--- scripts/gengraph.py
+++ scripts/gengraph.py
@@ -6,29 +6,58 @@ import matplotlib.markers as markers
 import matplotlib.transforms as transforms
 import seaborn as sns
 import pandas as pd
+from typing import TypedDict
 
 import sys
 
-assert len(sys.argv) == 5
+# Usage:
+# python3 gengraph.py 'lambda x: x["guesses"]' guesses.png < data.json
+assert len(sys.argv) == 3
+
+# Input "data.json" should look like this:
+# [
+#   [ # puzzle 1
+#     [data1, data2, ...],  # base runs
+#     [data1, data2, ...]   # head runs
+#   ],
+#   ...
+# ]
+
+
+class RunData(TypedDict):
+    title: str
+    author: str
+    puzzle: str
+    guesses: int
+    solveElapsedTimeMs: int
+    createElapsedTimeMs: int
+
+
+PuzzleData = tuple[list[RunData], list[RunData]]
+Data = list[PuzzleData]
+data: list[PuzzleData] = json.load(sys.stdin)
 
 mapper_script = sys.argv[1]
-base = json.load(open(sys.argv[2]))
-head = json.load(open(sys.argv[3]))
-outfilename = sys.argv[4]
+outfilename = sys.argv[2]
 
 mapper = eval(mapper_script)
-basestats = list(map(mapper, base))
-headstats = list(map(mapper, head))
+
+
+def median(xs):
+    xs = sorted(xs)
+    return xs[(len(xs) - 1) // 2]
+
 
 compare_data = pd.DataFrame(
-    list(zip(basestats, headstats)),
-    basestats,
+    list(
+        zip(
+            (mapper(run) for puzzle in data for run in puzzle[0]),
+            (mapper(run) for puzzle in data for run in puzzle[1]),
+        )
+    ),
+    [median(mapper(run) for run in puzzle[0]) for puzzle in data for run in puzzle[0]],
     ["base", "head"],
 )
-
-
-sortedbasestats = sorted(basestats)
-sortedheadstats = sorted(headstats)
 
 sns.set(style="whitegrid")
 fig, axd = plt.subplot_mosaic(
@@ -66,7 +95,12 @@ ax = sns.scatterplot(
 
 ax = sns.lineplot(
     data=pd.DataFrame(
-        list(zip([i + 1 for i in range(len(sortedbasestats))], sortedbasestats)),
+        list(
+            zip(
+                [i + 1 for i in range(len(data))],
+                sorted(median(mapper(run) for run in puzzle[0]) for puzzle in data),
+            )
+        ),
         columns=["y", "x"],
     ),
     y="y",
@@ -77,7 +111,12 @@ ax = sns.lineplot(
 )
 ax = sns.lineplot(
     data=pd.DataFrame(
-        list(zip([i + 1 for i in range(len(sortedheadstats))], sortedheadstats)),
+        list(
+            zip(
+                [i + 1 for i in range(len(data))],
+                sorted(median(mapper(run) for run in puzzle[1]) for puzzle in data),
+            )
+        ),
         columns=["y", "x"],
     ),
     y="y",
@@ -86,6 +125,6 @@ ax = sns.lineplot(
     markeredgecolor="none",
     ax=axd["bottom"],
 )
-ax.set_ylim(bottom=len(sortedbasestats) // 2, top=len(sortedbasestats) + 1)
+ax.set_ylim(bottom=len(data) // 2, top=len(data) + 1)
 
 plt.savefig(outfilename, format="png")
